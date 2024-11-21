d8db97a8dc9af41f64985ddd219ab7e9102a71a5
Ata ur rehman <68796785+ataaaaur@users.noreply.github.com>
2024-11-22 01:32:58 +0300

    Update weather_processing.h

--- weather_processing.h
+++ weather_processing.h
@@ -1,4 +1,3 @@
-// weather_processing.h
 #ifndef WEATHER_PROCESSING_H
 #define WEATHER_PROCESSING_H
 
@@ -6,12 +5,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define RAW_DATA_FILE "C:\\Users\\asras\\OneDrive\\Desktop\\3 sem\\CEW OEL\\rawdata.txt"
-#define PROCESSED_DATA_FILE "C:\\Users\\asras\\OneDrive\\Desktop\\3 sem\\CEW OEL\\processeddata.txt"
+#define RAW_DATA_FILE "rawdata.txt"
+#define PROCESSED_DATA_FILE "processeddata.txt"
 
 // Function declarations
 void get_current_datetime(char *buffer, size_t size);
 void process_weather_data();
 
 #endif // WEATHER_PROCESSING_H
-
